b20e43f70fd7686a6d79e957f556b56b10a3663a
ahibert <ahibertjr@gmail.com>
2019-04-27 01:57:51 +0100

    database code works, integration pending

--- datacollection/__init__.py
+++ datacollection/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from wtf_tinymce import wtf_tinymce
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 wtf_tinymce.init_app(app)
 app.secret_key = 'flkjsdfF7348503N=[F-0O3I4URasdfa7U8D54ferP4]WEOIjh45987wiehgh345ldfgn1ksyioneEkiwerRIGOQ'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
 from datacollection import routes
--- datacollection/models.py
+++ datacollection/models.py
@@ -1,5 +1,54 @@
-from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from datacollection import db
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    texts = db.relationship('Texts', backref='author', lazy=True)
+    text_versions = db.relationship('TextVersions', backref='author', lazy=True)
+    user_actions = db.relationship('UserActions', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.id}')"
+
+
+class Texts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}')"
+
+
+class TextVersions(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    fecha = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    text_id = db.Column(db.Integer, db.ForeignKey('texts.id'), nullable=False)
+
+    def __repr__(self):
+        return f"TextVersions('{self.id}')"
+
+
+class Actions(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    action = db.Column(db.String(10), nullable=False)
+
+    def __repr__(self):
+        return f"Actions('{self.action}')"
+
+
+class UserActions(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fecha = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    action = db.Column(db.Integer, db.ForeignKey('actions.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"UserActions('{self.user_id}', '{self.action}')"
