8a6ffd008c95ba91a06aa32ce62896ae0a5f2f4f
ahibert <ahibertjr@gmail.com>
2019-04-27 02:26:03 +0100

    fixed threading problem

--- datacollection/__init__.py
+++ datacollection/__init__.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 wtf_tinymce.init_app(app)
 app.secret_key = 'flkjsdfF7348503N=[F-0O3I4URasdfa7U8D54ferP4]WEOIjh45987wiehgh345ldfgn1ksyioneEkiwerRIGOQ'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db?check_same_thread=False'
 db = SQLAlchemy(app)
 
 from datacollection import routes
