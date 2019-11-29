d8e84e96684786eaf35a736cf51b909e7afd1d73
annie-primera <33452793+annie-primera@users.noreply.github.com>
2019-11-29 09:07:57 +0000

    Update __init__.py

--- datacollection/__init__.py
+++ datacollection/__init__.py
@@ -7,8 +7,9 @@ import ProWritingAidSDK
 
 app = Flask(__name__)
 
-app.secret_key = ''
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db?check_same_thread=False'
+app.secret_key = 'flkjsdfF7348503NF0O3I4URasdfa7U8D54ferP4]WEOIjh45987wiehgh345ldfgn1ksyioneEkiwerRIGOQ'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://wwwgrammaraidedu_ahibert:Sqlpr9.dl4eNaq@localhost/wwwgrammaraidedu_datacollection'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
@@ -19,14 +20,14 @@ app.config.update(dict(
     MAIL_PORT = 587,
     MAIL_USE_TLS = True,
     MAIL_USE_SSL = False,
-    MAIL_USERNAME = 'ahibertjr@gmail.com',
+    MAIL_USERNAME = '',
     MAIL_PASSWORD = '',
 ))
 mail = Mail(app)
 
-
 configuration = ProWritingAidSDK.Configuration()
 configuration.host = 'https://api.prowritingaid.com'
-configuration.api_key['licenseCode'] = ''
+configuration.api_key['licenseCode'] = 'A17D00BF-3DF2-40DA-AE0F-0B8172F2CB1C'
 
 from datacollection import routes
+
